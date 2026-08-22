bef5eb2fbeac1fa2324e63211e946896b97c2030
Surya Pratap Singh <suryapratap.singh1511@gmail.com>
2026-08-22 18:09:48 +0530

    Time: 109 ms (65.18%) | Memory: 8.9 MB (85.22%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.length();
+        int sum = 0;
+
+        for(int i=0;i<n;i++) {
+           int freq[26] = {0};
+
+            for(int j=i;j<n;j++) {
+
+                freq[s[j] - 'a'] ++;
+
+                int maxi = 0;
+                int mini = INT_MAX;
+
+                for(int k=0;k<26;k++) {
+                    if(freq[k] > 0) {
+                        mini = min(mini, freq[k]);
+                        maxi = max(maxi, freq[k]);
+                    }
+                }
+
+                sum += maxi - mini;
+            }
+        }
+
+        return sum;
+    }
+};
